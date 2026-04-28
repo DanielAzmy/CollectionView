a5615e8f8de779f2aa108ae7a52b4e04e1d03848
daniel-halan <daniel.azmy@halan.com>
2026-04-28 16:06:14 +0300

    adding Main viewController

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,12 +8,181 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Variables
+    private var images: [UIImage] = []
 
+    // MARK: - UI components
+    private lazy var stackView: UIStackView = {
+       let stack = UIStackView(arrangedSubviews: [titlee, subTitle])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 20
+        stack.alignment = .center
+        return stack
+    }()
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionHeadersPinToVisibleBounds = true
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeLayout())
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        
+        
+        return collectionView
+    }()
+    
+    private let titlee: UILabel = {
+       let label = UILabel()
+        label.text = "Collection View"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let subTitle: UILabel = {
+       let label = UILabel()
+        label.text = "Stack View"
+        label.textColor = .systemTeal
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
+        for _ in 0...25 {
+            images.append(UIImage(named: "1")!)
+            images.append(UIImage(named: "2")!)
+            images.append(UIImage(named: "3")!)
+            images.append(UIImage(named: "4")!)
+            images.append(UIImage(named: "5")!)
+        }
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(
+            CustomCollectionHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: CustomCollectionHeaderView.identifier
+        )
     }
+    
+    private func setupUI() {
+        view.addSubview(stackView)
+        view.addSubview(collectionView)
+        view.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: collectionView.topAnchor, constant: -20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            
+            collectionView.topAnchor.constraint(equalTo: stackView.bottomAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor) 
+        ])
+    }
+    
+    private func makeLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { _, _ in
+            // Item
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/3),
+                heightDimension: .fractionalWidth(1/3)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+            
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(1/3)
+            )
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
+            let section = NSCollectionLayoutSection(group: group)
 
+            let headerSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(60)
+            )
+            let header = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top,
+                absoluteOffset: CGPoint(x: 0, y: 60)
+                
+            )
+            header.pinToVisibleBounds = true
+            section.boundarySupplementaryItems = [header]
+            
+            return section
+        }
+    }
 }
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath)
+                as? CustomCollectionViewCell else {
+            fatalError("failed to dequeue cell")
+        }
+        let image = images[indexPath.row]
+        cell.configure(image: image)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (self.view.frame.width / 3) - 1.34
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return  2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader else {
+                return UICollectionReusableView()
+            }
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: CustomCollectionHeaderView.identifier, for: indexPath) as! CustomCollectionHeaderView
+        header.configure(title: "Header Title", subtitle: "subtitle")
+        return header
+
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 60)
+    }
+}
+
+
+
 
